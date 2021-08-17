acad1b9680a08ad38ae11a621f41489da6bd9479
JamesDoane <ijamesdoaneok@gmail.com>
2021-08-17 14:34:30 -0600

    Fully functional 'real estate' site

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
+const ctrl = require('./controller.js')
+
+app.get('/api/houses', ctrl.getHouses)
+
+app.post('/api/houses', ctrl.createHouse)
+
+app.put('/api/houses/:id', ctrl.updateHouse)
+
+app.delete('/api/houses/:id',ctrl.deleteHouse)
+
+app.listen(4004, ()=> console.log("listening on 4004"))
